9673299305dc362b3c8e089ccce3e535de80a33c
Panji <panji9502@gmail.com>
2022-12-22 21:49:11 +0700

    Adding Local Storage

--- src/contexts/books.context.jsx
+++ src/contexts/books.context.jsx
@@ -1,11 +1,16 @@
-import React, { createContext, useReducer, useState } from "react";
+import React, { createContext, useEffect, useReducer } from "react";
 import { bookReducer } from "../reducers/book.reducer";
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [books, dispatch] = useReducer(bookReducer, []);
-
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const localData = localStorage.getItem("books");
+    return localData ? JSON.parse(localData) : [];
+  });
+  useEffect(() => {
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]);
   return (
     <BookContext.Provider value={{ books, dispatch }}>
       {props.children}
